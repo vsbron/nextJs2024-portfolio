2be24bc646635238bef5f47b481da9728cbac91c
BroN <you@example.comvsbron@gmail.com>
2024-08-21 11:59:13 +0300

    Added footer section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { navItems } from "@/data";
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -19,6 +20,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
